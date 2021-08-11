408601afc6947ed5e9fee869125f645cd8bb1f1c
praveen <praveenisc@gmail.com>
2021-08-11 18:20:58 +0530

    second commit

--- 15. Token Authentication using Signal/lacture/api/views.py
+++ 15. Token Authentication using Signal/lacture/api/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from .serializers import StudentSerializer
-from .models import Student
 from rest_framework import viewsets
+import rest_framework
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
 
 class StudentApi(viewsets.ModelViewSet):
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
--- 15. Token Authentication using Signal/lacture/lacture/settings.py
+++ 15. Token Authentication using Signal/lacture/lacture/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d22woct7f6dh+8p&x)8&zh@#=o@4%zy&w7eplaj0rwczgrw*ej'
+SECRET_KEY = 'django-insecure-9$-t+e@%^mfg9cbe*zyyecg=19a&u94gpnc#a@#(%x2fm38&y$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'api'
+    'api',
 ]
 
 MIDDLEWARE = [
